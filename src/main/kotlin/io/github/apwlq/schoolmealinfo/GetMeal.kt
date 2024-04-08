aafc8d55345e31d2af91e323ab44d34c548b9503
apwlq <makerzip@hanmail.net>
2024-04-08 14:32:16 +0900

    :fire: 급식 정규식 핫픽스

--- src/main/kotlin/io/github/apwlq/schoolmealinfo/GetMeal.kt
+++ src/main/kotlin/io/github/apwlq/schoolmealinfo/GetMeal.kt
@@ -2,15 +2,15 @@ package io.github.apwlq.schoolmealinfo
 
 import io.github.apwlq.mealapi.NeisApi
 
-val filter = "[^\\uAC00-\\uD7A3\\n]".toRegex()
-val customFilter = "공산|공산품|다선|하단|하단중|주찬|덕".toRegex()
+val filter = "\\([^)]*\\)|[0-9]|&\\n".toRegex()
+val customFilter = "공산|공산품|다선|하단|하단중|주찬|덕|하|대".toRegex()
 
 fun getBreakfast(): String? {
     val neis = NeisApi.Builder().build()
     val schoolName = System.getenv("SCHOOL_NAME")
     val sch = neis.getSchoolByName(schoolName).first()
     val meal = neis.getMealsByDay(getNowDate(), sch.scCode, sch.schoolCode)
-    return meal.breakfast?.joinToString("\n")?.replace(filter, "")?.replace(customFilter, "")?.replace("&".toRegex(), "\n")
+    return meal.breakfast?.joinToString("\n")?.replace(filter, "")?.replace(customFilter, "")?.replace(Regex("&(\r?\n|$)"), "$1")
 }
 
 fun getLunch(): String? {
@@ -18,7 +18,7 @@ fun getLunch(): String? {
     val schoolName = System.getenv("SCHOOL_NAME")
     val sch = neis.getSchoolByName(schoolName).first()
     val meal = neis.getMealsByDay(getNowDate(), sch.scCode, sch.schoolCode)
-    return meal.lunch?.joinToString("\n")?.replace(filter, "")?.replace(customFilter, "")?.replace("&".toRegex(), "\n")
+    return meal.lunch?.joinToString("\n")?.replace(filter, "")?.replace(customFilter, "")?.replace(Regex("&(\r?\n|$)"), "$1")
 }
 
 fun getDinner(): String? {
@@ -26,7 +26,7 @@ fun getDinner(): String? {
     val schoolName = System.getenv("SCHOOL_NAME")
     val sch = neis.getSchoolByName(schoolName).first()
     val meal = neis.getMealsByDay(getNowDate(), sch.scCode, sch.schoolCode)
-    return meal.dinner?.joinToString("\n")?.replace(filter, "")?.replace(customFilter, "")?.replace("&".toRegex(), "\n")
+    return meal.dinner?.joinToString("\n")?.replace(filter, "")?.replace(customFilter, "")?.replace(Regex("&(\r?\n|$)"), "$1")
 }
 
 
